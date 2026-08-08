a595c6c03cac90a8c9d4c74732c9d3232775450a
Ashish Chandra <ashishchandra157@gmail.com>
2026-08-08 21:22:34 +0200

    featz : add meter reading data model

---
+++ firmware/MeterBridge_FW/Core/Inc/meter.h
@@ -0,0 +1,23 @@
+/*
+ * meter.h
+ *
+ *  Created on: Aug 8, 2026
+ *      Author: ashish
+ */
+
+#ifndef INC_METER_H_
+#define INC_METER_H_
+
+#include <stdint.h>
+
+typedef struct
+{
+    uint32_t voltage_mV;
+    uint32_t current_mA;
+    uint32_t power_mW;
+    uint32_t energy_mWh;
+} MeterReading;
+
+
+
+#endif /* INC_METER_H_ */
---
+++ firmware/MeterBridge_FW/Core/Src/meter.c
@@ -0,0 +1,9 @@
+/*
+ * meter.c
+ *
+ *  Created on: Aug 8, 2026
+ *      Author: ashish
+ */
+
+#include "meter.h"
+
